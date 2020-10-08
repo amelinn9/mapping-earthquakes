9ad4031296e06de5db0674b8773b57e765db1ac3
amelin <anorzamini@gmail.com>
2020-10-08 13:22:21 -0700

    updated code

--- earthquakes_past7days/static/js/logicStep1.js
+++ earthquakes_past7days/static/js/logicStep1.js
@@ -68,22 +68,22 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with a center and zoom level. 
 // useful when we need to add multiple tile layers or background image of our map(s)
 let map = L.map("mapid", {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/amelinn9/mapping-earthquakes/main/torontoNeighborhoods.json";
+/// Retrieve the earthquake GeoJSON data.
+let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Create a style for the lines.
 let myStyle = {
@@ -93,17 +93,24 @@ let myStyle = {
 };
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakes).then(function(data) {
   console.log(data);
 
   L.geoJson(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
     console.log(layer);
-    layer.bindPopup("<h3>Neighbourhood: " + feature.properties.AREA_NAME + "</h3>");
+    layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3> <hr> <h3>Magnitude: " +
+    feature.properties.mag + "</h3>");
     }
   }).addTo(map);
 });
+
+
+//let myTime = new Date(features.properties.time *1000);
+//console.log(myTime);
+// <h3>Date: " + feature.properties.time(myTime.toLocaleString()) + </h3>
+
 
 // FOR line markers
 // Grabbing our GeoJSON data.
